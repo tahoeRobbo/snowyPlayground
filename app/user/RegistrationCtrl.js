bfe7a3a9fae3c159150f66ba03f1af50f7d8ebd0
tahoeRobbo <rob@snowyplayground.com>
2015-06-28 18:54:12 -0600

    got the login/out tmpl and functionality working! good day, stoked stoked stoked

--- app/user/RegistrationCtrl.js
+++ app/user/RegistrationCtrl.js
@@ -1,13 +1,35 @@
 var app = angular.module('snowyPlayground');
 
-app.controller('RegistrationCtrl', function($scope, RegistrationService) {
+app.controller('RegistrationCtrl', function($scope, RegistrationService, $location) {
 	$scope.test = 'Hello World from the RegistrationCtrl';
 	$scope.loginTest = "YO YO YO DIS BE DAT LOGIN VIEW BOIIIII";
 	$scope.registrationTest = "MAN THIS SHIT FIN BE OFF DA CHAINS!";
 	
+	//LOGIN
+	$scope.login = function() {
+		console.log('RegistrationCtrl $scope.login HIT');
+		RegistrationService.loginUser($scope.user);
+		$location.path('/profile');
+	};
+	
 	//REGISTER
 	$scope.register = function() {
 		console.log('RegistrationCtrl $scope.register Hit!');
-		RegistrationService.registerUser($scope.user);
-	};
+		RegistrationService.registerUser($scope.user)
+		//Register User
+		.then(function() {
+			RegistrationService.loginUser($scope.user);
+			$location.path('/profile');
+		})// AND THEN login
+			.catch(function(error){
+			$scope.message = error.message;
+		});// error catch
+	}; // End $scope.register
+	
+	//LOGOUT
+	$scope.logout = function() {
+		console.log('RegistrationCtrl $scope.logout HIT!');
+		RegistrationService.logoutUser();
+		$location.path('/login')
+	};//End $scope.logout
 });
--- app/user/RegistrationService.js
+++ app/user/RegistrationService.js
@@ -5,6 +5,16 @@ app.service('RegistrationService', function($rootScope, $firebaseObject, $fireba
 	var ref = new Firebase(FBURL);
 	var auth = $firebaseAuth(ref);
 	
+	
+	this.loginUser = function(user) {
+		console.log('RegistrationCtrl this.loginUser HIT');
+		return auth.$authWithPassword({
+			email : user.email,
+			password : user.password
+		});
+	};//End this.loginUser
+	
+	
 	this.registerUser = function(user) {
 		console.log('RegistrationService this.registerUser Hit')
 		return auth.$createUser({
@@ -22,6 +32,26 @@ app.service('RegistrationService', function($rootScope, $firebaseObject, $fireba
 			firebaseUsers.email = user.email;
 			
 			firebaseUsers.$save();
+			
 		}); //end adding user info into firebase (AND THEN)
 	};// End this.registerUser
+	
+	auth.$onAuth(function(authUser){
+		if(authUser) {
+			var onRef = new Firebase(FBURL + "/users/" + authUser.uid);
+			var userData = $firebaseObject(onRef);
+			userData.$loaded().then(function(fullUser) {
+				$rootScope.currentUser = fullUser;
+				console.log("Logged in as ", $rootScope.currentUser);
+			});//If logging in, return full user from firebase and assign it to $rS.cU
+		} else {
+			$rootScope.currentUser = '';
+			console.log("Logged Out");
+		}// If loggin out, clear $rootScope.currentUser
+	}); // end auth.$onAuth
+
+	this.logoutUser = function() {
+		return auth.$unauth();
+	}; // end this.logout
+	
 });// End RegistrationService
