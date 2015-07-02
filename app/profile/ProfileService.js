4a0ff0ab745eb58ab5eb89688cea2abde9660309
tahoeRobbo <rob@snowyplayground.com>
2015-07-01 23:52:01 -0600

    started working on master; commiting and switching to profile

--- app/profile/ProfileService.js
+++ app/profile/ProfileService.js
@@ -2,16 +2,24 @@ var app = angular.module('snowyPlayground');
 
 app.service('ProfileService', function($rootScope, $firebaseObject, FBURL){
 	
+	//**Changed all refs' to userRef and userObj
+	
 	this.addSummary = function(user) {
 		console.log('service addStory Hit');
-		var storyRef = new Firebase(FBURL + '/users/' + $rootScope.currentUser.uid);
-		var firebaseProfile = $firebaseObject(storyRef);
+		var userRef = new Firebase(FBURL + '/users/' + $rootScope.currentUser.uid);
+		var userObj = $firebaseObject(userRef);
 		
-		firebaseProfile.$loaded(function(){
-				firebaseProfile.summary = user.summary;
-			firebaseProfile.$save();
-});
-			
+		userObj.$loaded(function(){
+				userObj.summary = user.summary;
+			userObj.$save();//after the fbObj is loaded, add new key/val
+});// end this.addSummary
+		
+		this.addPic = function(user) {
+			console.log('service addPic HIT');
+			var userRef = new Firebase(FBURL + '/users/' + $rootScope.currentUser.uid);
+		var  = $firebaseObject(userRef);
+		}
+		
 	};
 	
 });//End Profile Service
