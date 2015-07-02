39bb3d50eee739c11c7991244dd118ed745d0496
tahoeRobbo <rob@snowyplayground.com>
2015-07-02 15:36:45 -0600

    added chat function, said by userName w/picture -- need to figure out how to reference userPic rather than embed pic in message

--- app/chat/ChatCtrl.js
+++ app/chat/ChatCtrl.js
@@ -1,5 +1,20 @@
 var app = angular.module('snowyPlayground');
 
-app.controller('ChatCtrl', function($scope) {
+app.controller('ChatCtrl', function($scope, ChatService, FBURL, $firebaseArray) {
 	$scope.chatTest = "FIN BE CHATTIN WITH IMAGES N SHIT";
-});
+	
+	var ref = new Firebase(FBURL + '/messages');
+	var fbMessages = $firebaseArray(ref);
+	$scope.messages=fbMessages;
+	
+	console.log($scope.messages);
+	$scope.addMessage = function() {
+		ChatService.addMessage($scope.message)
+		.then(function() {
+			$scope.message = '';
+		});//end 
+	};//end $scope.addMessage
+	
+
+
+});//end ChatCtrl
--- app/chat/ChatService.js
+++ app/chat/ChatService.js
@@ -1,5 +1,18 @@
 var app = angular.module('snowyPlayground');
 
-app.service('ChatService', function() {
+app.service('ChatService', function($firebaseArray, $rootScope, FBURL) {
 	
-});
+		var ref = new Firebase(FBURL + '/messages');
+		var fbMessages = $firebaseArray(ref);
+		
+	
+	this.addMessage = function(message){
+		return fbMessages.$add({
+			message : message.text,
+			user : $rootScope.currentUser.userName,
+			userPic : $rootScope.currentUser.picData
+		});
+	};
+
+	
+});//end ChatService
